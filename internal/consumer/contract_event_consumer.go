2ec1f66f0cd38409cb22af5b14e555c90f76cf0d
Allyson-English <allyson.english@gmail.com>
2025-03-05 11:50:27 -0500

    file org

--- internal/consumer/contract_event_consumer.go
+++ internal/consumer/contract_event_consumer.go
@@ -17,9 +17,11 @@ import (
 )
 
 const (
-	teslaIntegrationID        = 2
-	contractEventType         = "zone.dimo.contract.event"
-	syntheticDeviceNodeMinted = "SyntheticDeviceNodeMinted"
+	teslaIntegrationID               = 2
+	contractEventType                = "zone.dimo.contract.event"
+	syntheticDeviceNodeMinted        = "SyntheticDeviceNodeMinted"
+	syntheticDeviceNodeMintedEventID = "0x5a560c1adda92bd6cbf9c891dc38e9e2973b7963493f2364caa40a4218346280"
+	syntheticDeviceNodeBurnedEventID = "0xe4edc3c1f917608d486e02df63af34158f185b78cef44615aebee26c09064122"
 )
 
 type contractEventData struct {
@@ -31,18 +33,18 @@ type contractEventData struct {
 type Processor struct {
 	pdb                              db.Store
 	logger                           *zerolog.Logger
-	syntheticDeviceNodeMintedEventID common.Hash
+	syntheticDeviceNodeMintedEventID string
+	syntheticDeviceNodeBurnedEventID string
 }
 
 func New(
 	pdb db.Store,
 	logger *zerolog.Logger) *Processor {
-	syntheticDeviceNodeMintedEventID := common.HexToHash("0x5a560c1adda92bd6cbf9c891dc38e9e2973b7963493f2364caa40a4218346280")
-
 	return &Processor{
 		pdb:                              pdb,
 		logger:                           logger,
 		syntheticDeviceNodeMintedEventID: syntheticDeviceNodeMintedEventID,
+		syntheticDeviceNodeBurnedEventID: syntheticDeviceNodeBurnedEventID,
 	}
 }
 
@@ -72,8 +74,14 @@ func (p Processor) ConsumeClaim(session sarama.ConsumerGroupSession, claim saram
 			}
 
 			switch event.Data.EventSignature {
-			case p.syntheticDeviceNodeMintedEventID.Hex():
+			case p.syntheticDeviceNodeMintedEventID:
 				if err := p.handleSyntheticDeviceNodeMinted(session.Context(), event.Data.Arguments); err != nil {
+					p.logger.Err(err).Msg("failed to process tesla device mint")
+					continue
+				}
+				session.MarkMessage(msg, "")
+			case p.syntheticDeviceNodeBurnedEventID:
+				if err := p.handleSyntheticDeviceNodeBurned(session.Context(), event.Data.Arguments); err != nil {
 					p.logger.Err(err).Msg("failed to process tesla device mint")
 					continue
 				}
@@ -85,6 +93,14 @@ func (p Processor) ConsumeClaim(session sarama.ConsumerGroupSession, claim saram
 
 		}
 	}
+}
+
+type SyntheticDeviceNodeMinted struct {
+	IntegrationNode        *big.Int       `json:"integrationNode"`
+	SyntheticDeviceNode    *big.Int       `json:"syntheticDeviceNode"`
+	VehicleNode            *big.Int       `json:"vehicleNode"`
+	SyntheticDeviceAddress common.Address `json:"syntheticDeviceAddress"`
+	Owner                  common.Address `json:"owner"`
 }
 
 func (p Processor) handleSyntheticDeviceNodeMinted(ctx context.Context, data json.RawMessage) error {
@@ -113,10 +129,29 @@ func (p Processor) handleSyntheticDeviceNodeMinted(ctx context.Context, data jso
 	return fmt.Errorf("failed to update table for sythetic device %s: %w", partSynthDev.Address, err)
 }
 
-type SyntheticDeviceNodeMinted struct {
-	IntegrationNode        *big.Int       `json:"integrationNode"`
-	SyntheticDeviceNode    *big.Int       `json:"syntheticDeviceNode"`
-	VehicleNode            *big.Int       `json:"vehicleNode"`
-	SyntheticDeviceAddress common.Address `json:"syntheticDeviceAddress"`
-	Owner                  common.Address `json:"owner"`
+type SyntheticDeviceNodeBurned struct {
+	SyntheticDeviceNode *big.Int       `json:"syntheticDeviceNode"`
+	VehicleNode         *big.Int       `json:"vehicleNode"`
+	Owner               common.Address `json:"owner"`
+}
+
+func (p Processor) handleSyntheticDeviceNodeBurned(ctx context.Context, data json.RawMessage) error {
+	var args SyntheticDeviceNodeBurned
+	if err := json.Unmarshal(data, &args); err != nil {
+		return fmt.Errorf("failed to parse device burn event: %w", err)
+	}
+
+	delCount, err := models.SyntheticDevices(
+		models.SyntheticDeviceWhere.TokenID.EQ(
+			null.IntFrom(int(args.SyntheticDeviceNode.Int64()))),
+	).DeleteAll(ctx, p.pdb.DBS().Writer)
+	if err != nil {
+		return fmt.Errorf("failed to delete synthetic device node %d: %w", args.SyntheticDeviceNode.Int64(), err)
+	}
+
+	if delCount != 1 {
+		return fmt.Errorf("synthetic device node %d burn even deleted unexpected number of devices: %d", args.SyntheticDeviceNode.Int64(), delCount)
+	}
+
+	return nil
 }
