0d122e581761287b1386b25f5286009ab67374db
Dylan Moreland <dylan@dimo.zone>
2025-08-23 20:28:35 -0400

    Do a little log when devices registers

--- internal/rpc/tesla_service.go
+++ internal/rpc/tesla_service.go
@@ -111,6 +111,8 @@ func (t *TeslaRPCService) RegisterNewSyntheticDeviceV2(ctx context.Context, req 
 		return nil, fmt.Errorf("couldn't insert synthetic device record: %w", err)
 	}
 
+	t.logger.Info().Str("vin", req.Vin).Str("address", sdAddr.Hex()).Msgf("Creating pre-synthetic for devices-api.")
+
 	return &grpc.RegisterNewSyntheticDeviceV2Response{
 		SyntheticDeviceAddress: sdAddr.Bytes(),
 		WalletChildNum:         walletIndexBounded,
