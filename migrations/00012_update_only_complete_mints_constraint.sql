173c915baf1534c082c5d921fb93e5408dfdf5d2
dmytrozakharenko <amidum@gmail.com>
2025-10-24 11:04:00 -0400

    -  added DISALLOWED state comment

--- migrations/00012_update_only_complete_mints_constraint.sql
+++ migrations/00012_update_only_complete_mints_constraint.sql
@@ -2,10 +2,14 @@
 -- +goose StatementBegin
 -- Update only_complete_mints constraint to allow disconnected state
 -- Old: (vehicle_token_id IS NULL AND token_id IS NULL) OR (vehicle_token_id IS NOT NULL AND token_id IS NOT NULL)
--- New: Allow three states:
---   1. Not minted: both NULL
---   2. Fully minted: both NOT NULL
---   3. Disconnected: vehicle_token_id NOT NULL, token_id NULL
+-- New: Allow three valid states:
+--   1. Not minted: both NULL (initial state before any minting)
+--   2. Fully connected: both NOT NULL (vehicle and SD both minted and connected)
+--   3. Disconnected: vehicle_token_id NOT NULL, token_id NULL (SD burned, vehicle preserved for reconnection)
+--
+-- Intentionally DISALLOWED state:
+--   4. Invalid: vehicle_token_id IS NULL AND token_id IS NOT NULL
+--      (Cannot have SD without Vehicle - violates business logic)
 ALTER TABLE tesla_oracle.synthetic_devices
 DROP CONSTRAINT only_complete_mints;
 
